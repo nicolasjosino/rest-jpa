9502c60cb2c1ef8121a954b8fbdb89d72b1a8626
Nicolas Josino <nicolasjosino@gmail.com>
2021-11-30 17:41:57 -0300

    Ajuste no projeto.

--- rest-jpa/src/main/java/br/edu/uni7/tecnicas/restjpa/UsuarioBusiness.java
+++
@@ -1,9 +0,0 @@
-package br.edu.uni7.tecnicas.restjpa;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UsuarioBusiness {
-	public boolean autenticar(String login, String senha);
-	public void salvar(Usuario usuario);
-}
