a3d4a00fbb83427eb6743654e0fbe243fa3ec125
Nicolas Josino <nicolasjosino@gmail.com>
2021-11-30 17:55:55 -0300

    Correção na interface UsuarioRepositorio.

--- src/main/java/br/edu/uni7/tecnicas/restjpa/UsuarioRepositorio.java
+++ src/main/java/br/edu/uni7/tecnicas/restjpa/UsuarioRepositorio.java
@@ -3,7 +3,10 @@ package br.edu.uni7.tecnicas.restjpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.edu.uni7.tecnicas.restjpa.Usuario.StatusType;
+
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
 	public Usuario findByLogin(String login);
+	public Usuario findByLoginAndStatus(String login, StatusType status);
 }
